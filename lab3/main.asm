100fd4544b4a9024ec6ab0b23339ab3f3b4fc1d1
Nathan <nathansoo99@gmail.com>
2024-10-21 12:42:50 +1100

    finished input read code

--- lab3/main.asm
+++ lab3/main.asm
@@ -9,19 +9,24 @@
 .equ PORTBDIR		= 0b00001111
 .equ INITCOLMASK	= 0b01111111
 .equ INITROWMASK	= 0b00001000
-.equ COLXORMASK		= 0b10000000
 .equ CHECKROWMASK	= 0b00001111
 
 .equ KEYPADSIZE		= 4
+.equ NSTATES		= 3
+.equ LARGESTDIGIT	= 9
 
 .def w				= r16				; free to change
-.def cur_col		= r2				; free to change
-.def cur_row		= r3				; free to change
-.def col_xor_mask	= r4				; please don't change
-.def keypad_size	= r5				; please don't change
-.def row_val		= r6				; free to change
-.def row_mask		= r17				; free to change
-.def col_mask		= r18				; free to change
+.def cur_col		= r2				; free to change outside of keypad loop
+.def cur_row		= r3				; free to change outside of keypad loop
+.def keypad_size	= r4				; please don't change
+.def row_val		= r5				; free to change outside of keypad loop
+.def temp			= r6				; free to change
+.def arg0			= r10				; please don't change
+.def arg1			= r11				; please don't change
+.def arg2			= r12				; please don't change
+.def row_mask		= r17				; please don't change
+.def col_mask		= r18				; free to change outside of keypad loop
+.def state			= r19				; please don't change
 
 .cseg
 
@@ -34,11 +39,29 @@ debounce_loop:
 	subi zl, 1
 	sbci zh, 0
 	brne debounce_loop
-
 .endmacro
 
-	ldi		w, COLXORMASK				; initialise xor mask
-	mov		col_xor_mask, w
+.macro add_digit
+	mov		r6, @0
+	lsl		r6
+	lsl		r6
+	lsl		r6
+	lsl		@0
+	add		@0, r6
+	add		@0, @1
+.endmacro
+
+.macro advance_state
+	inc		state
+	cpi		state, NSTATES
+	brlt	end_advance
+	clr		state
+	clr		arg0
+	clr		arg1
+	clr		arg2
+end_advance:
+.endmacro
+	
 
 	ldi		w, KEYPADSIZE				; initialise keypad size
 	mov		keypad_size, w
@@ -47,6 +70,11 @@ debounce_loop:
 	out		DDRC, w
 	ldi		w, PORTBDIR
 	out		DDRB, w
+
+	clr		state						; initialise input state
+	clr		arg0
+	clr		arg1
+	clr		arg2
 
 scan_keypad:
 	clr		cur_col						; clear column
@@ -87,32 +115,80 @@ inc_row:
 	brne	row_loop
 nextcol:
 	lsr		col_mask					; shift column mask right
-	eor		col_mask, col_xor_mask		; re-set left most bit and clear bit right of mask
+	sbr		col_mask, 0b10000000		; re-set left most bit
 	inc		cur_col						; increment column count
 	rjmp	loop
 
 resolve:
-	lsl		cur_row
+	lsl		cur_row						; get key index by adding row * 4 + column
 	lsl		cur_row
 	mov		w, cur_row
 	add		w, cur_col
 
-	ldi		zh, high(lookup<<1)
-	ldi		zl, low(lookup<<1)
+	ldi		zh, high(value_lookup<<1)	; get numeric value from lookup table (or ascii if non-numeric)
+	ldi		zl, low(value_lookup<<1)
 
-	clr		r15
+	clr		r15							
 	add		zl, w
 	adc		zh, r15
 
 	lpm		w, z
 
-	out		PORTB, w
+	cpi		state, 0					; state machine
+	breq	state0
+	cpi		state, 1
+	breq	state1
+	cpi		state, 2
+	breq	state2
+
+	; below is where code to write to LCD can be written
+state0:
+	cpi		w, LARGESTDIGIT				; check if value is numeric
+	brge	not_num0					; if not check if button advances state
+	add_digit arg0, w					; if numeric then multiply existing argument by 10 and add new number
+	rjmp	end_state
+not_num0:
+	cpi		w, 0x2A						; check if correct button is pressed otherwise do nothing	
+	brne	end_state					
+	advance_state						; otherwise advance the state machine
+	rjmp	end_state
+state1:
+	cpi		w, LARGESTDIGIT
+	brge	not_num1
+	add_digit arg1, w
+	rjmp	end_state
+not_num1:
+	cpi		w, 0x44
+	brne	end_state
+	advance_state
+	rjmp	end_state
+state2:
+	cpi		w, LARGESTDIGIT
+	brge	not_num2
+	add_digit arg2, w
+	rjmp	end_state
+not_num2:
+	cpi		w, 0x23
+	brne	end_state
+	; call calculation function here arguments in r10, r11, r12 currently
+	advance_state						; note that advance state here will clear the r10, r11, r12
+end_state:
+
+	out		PORTB, arg2
+
+hold_loop:								; wait until button is unpressed before continuing to read keypad
+	in		w, PINC						; read port again
+	and		w, row_mask					; check value of pin again
+	breq	hold_loop					; if button still pressed don't allow key to be continually read
+
+	rjmp	scan_keypad
 
 
-	rjmp scan_keypad
+	
 
 halt:
 	rjmp	halt
-
-lookup:
-	.db	0x31, 0x32, 0x33, 0x41, 0x34, 0x35, 0x36, 0x42, 0x37, 0x38, 0x39, 0x43, 0x2A, 0x30, 0x23, 0x44
+value_lookup:
+	.db		1, 2, 3, 0x41, 4, 5, 6, 0x42, 7, 8, 9, 0x43, 0x2A, 0, 0x23, 0x44
+ascii_lookup:
+	.db		0x31, 0x32, 0x33, 0x41, 0x34, 0x35, 0x36, 0x42, 0x37, 0x38, 0x39, 0x43, 0x2A, 0x30, 0x23, 0x44
