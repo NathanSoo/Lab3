048ce02e2764df383b343dd0cd9299dc66e789d1
ZhaoSiyuan120090117 <118808860+ZhaoSiyuan120090117@users.noreply.github.com>
2024-10-24 13:48:10 +1100

    Update main.asm

--- Decimal/main.asm
+++ Decimal/main.asm
@@ -1,13 +1,12 @@
 ;
-; Decimal.asm
+; Decimal_Hex.asm
 ;
 ; Created: 22/10/2024 3:49:27 PM
 ; Author : Siyuan Zhao z5554919
 ;
 
 ;   r27 will be the ascii value of the hundreds and tens
-;   r25 will be the ascii value of the ones
-;   r28-R23 are used here
+;   r30 will be the ascii value of the ones
 	
 
 decimal_conversion:
@@ -27,7 +26,7 @@ positive_number:
     ; Convert hundreds place
     ldi r28, 100              ; Load 100 for division
     rcall divide               ; Call divide subroutine
-    mov r27, r24               ; Get the quotient (hundreds digit) in r27
+    mov r27, r29               ; Get the quotient (hundreds digit) in r27
     cpi r27, 0                ; Check if it's zero
     brne display_digit         ; If non-zero, display it
     rjmp check_tens_digit      ; Otherwise, move to tens place
@@ -40,7 +39,7 @@ check_tens_digit:
     ; Convert tens place
     ldi r28, 10               ; Load 10 for division
     rcall divide               ; Call divide subroutine
-    mov r27, r24               ; Get the quotient (tens digit) in r27
+    mov r27, r29               ; Get the quotient (tens digit) in r27
     cpi r27, 0                ; Check if it's zero
     brne display_tens_digit    ; If non-zero, display it
     rjmp display_ones_digit    ; Otherwise, move to ones place
@@ -50,25 +49,25 @@ display_tens_digit:
 ;    rcall lcd_send_data        ; Send tens digit to LCD
 
 display_ones_digit:
-    ; Ones place is left in r25 (remainder of tens division)
-    add r25, r23              ; Convert to ASCII
+    ; Ones place is left in r30 (remainder of tens division)
+    add r30, r23              ; Convert to ASCII
 ;    rcall lcd_send_data        ; Send ones digit to LCD
 
     ret                        ; Return from subroutine
 
-; Divide r26 by r28, result is quotient in r24 and remainder in r25
+; Divide r26 by r28, result is quotient in r29 and remainder in r30
 divide:
-    clr r24                   ; Clear quotient register
-    clr r25                   ; Clear remainder register
+    clr r29                   ; Clear quotient register
+    clr r30                   ; Clear remainder register
 
 divide_loop:
     cp r26, r28               ; Compare r26 (dividend) with r28 (divisor)
     brlo divide_done           ; If r26 < r28, exit loop
 
     sub r26, r28              ; Subtract divisor from dividend
-    inc r24                   ; Increment quotient
+    inc r29                   ; Increment quotient
     rjmp divide_loop           ; Repeat until r26 < r28
 
 divide_done:
-    mov r25, r26              ; Store remainder in r25
-    ret                        ; Return with quotient in r24 and remainder in r25
+    mov r30, r26              ; Store remainder in r30
+    ret                        ; Return with quotient in r29 and remainder in r30
