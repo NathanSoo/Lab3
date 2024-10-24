ba9193ee400f4ac34351420185934d611f5d228c
ZhaoSiyuan120090117 <118808860+ZhaoSiyuan120090117@users.noreply.github.com>
2024-10-24 13:54:14 +1100

    Update main.asm

--- hexadecimal/main.asm
+++ hexadecimal/main.asm
@@ -5,13 +5,13 @@
 ; Author : Siyuan Zhao
 ;
 ; r23-r26 are used here
-; The ascii value will be stored in r25 and then sent to lcd
+; The ascii value will be stored in r27 and then sent to lcd
 
 
 ; Replace with your application code
 hex_conversion:
-	;lds r24, result
-    ldi r24, 97          ; Load result (y) into register r24. 97 for the test
+	;lds r29, result
+    ldi r29, 97          ; Load result (y) into register r29. 97 for the test
 
     ; Display '0x' prefix
     ldi r16, '0'
@@ -20,18 +20,18 @@ hex_conversion:
 ;    rcall lcd_send_data
 
     ; Extract high nibble
-    mov r25, r24          ; Copy number to r25
-    lsr r25               ; Shift right by 4 to get high nibble
-    lsr r25
-    lsr r25
-    lsr r25
-    andi r25, 0x0F        ; Mask to get lower 4 bits (high nibble)
+    mov r27, r29          ; Copy number to r27
+    lsr r27               ; Shift right by 4 to get high nibble
+    lsr r27
+    lsr r27
+    lsr r27
+    andi r27, 0x0F        ; Mask to get lower 4 bits (high nibble)
     rcall hex_to_ascii     ; Convert high nibble to ASCII
 ;    rcall lcd_send_data    ; Send to LCD
 
     ; Extract low nibble
-    mov r25, r24          ; Copy number to r25 again
-    andi r25, 0x0F        ; Mask to get low nibble
+    mov r27, r29          ; Copy number to r27 again
+    andi r27, 0x0F        ; Mask to get low nibble
     rcall hex_to_ascii     ; Convert low nibble to ASCII
 ;    rcall lcd_send_data    ; Send to LCD
 
@@ -39,14 +39,14 @@ hex_conversion:
 
 hex_to_ascii:
 	ldi r26, 'A'-10
-    cpi r25, 10           ; Compare if nibble is 0-9 or A-F
+    cpi r27, 10           ; Compare if nibble is 0-9 or A-F
     brlo is_digit          ; If less than 10, it's a digit
-    add r25, r26      ; Convert to 'A'-'F'
+    add r27, r26      ; Convert to 'A'-'F'
     rjmp done_hex_conversion
 
 is_digit:
 	ldi r23, '0'
-    add r25, r23           ; Convert to ASCII '0'-'9'
+    add r27, r23           ; Convert to ASCII '0'-'9'
 
 done_hex_conversion:
     ret
